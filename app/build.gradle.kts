6c10905a465622e53f130d1e02453f60b903bc19
Arbaz001 <arbaza3212btechcse2025@kccitm.edu.in>
2024-04-22 12:49:31 +0530

    day3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,10 @@ android {
 }
 
 dependencies {
-
+    // Kotlin
+    var nav_version = "2.7.7"
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
